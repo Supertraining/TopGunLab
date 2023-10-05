56ec700c10392007f5c0e8d05928e1e4b4c33dfc
Dindi Lamadrid <90066915+Dindilama@users.noreply.github.com>
2023-10-04 20:14:26 -0500

    Cambios en el html, css y js

--- js/index.js
+++ js/index.js
@@ -1,44 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-  const formulario = document.getElementById('formulario');
-
   const citasDiv = document.getElementById('citas');
+  const formulario = document.getElementById('formulario');
+  const nombreInput = document.getElementById('nombre');
+  const fechaHoraInput = document.getElementById('fechaHora');
+  const duracionCita = 5000; 
 
-  const agregarCitaBtn = document.getElementById('agregarCitaBtn');
+  formulario.addEventListener('submit', (event) => {
+      event.preventDefault();
 
+      // Obtiene datos del formulario
+      const nombre = nombreInput.value;
+      const fechaHora = fechaHoraInput.value;
 
-  agregarCitaBtn.addEventListener('click', () => {
-
-    const nombre = document.getElementById('nombre').value;
-
-    const fechaHora = document.getElementById('fechaHora').value;
-
-    if (nombre && fechaHora) {
-
-      // Crear una tarjeta de cita y agregarla al div de citas
-
-      const citaDiv = document.createElement('div');
-
-      citaDiv.className = 'cita';
-
-      citaDiv.innerHTML = `
-
+      // Verificación de ingreso de datos
+      if (nombre && fechaHora) {
+          // Creación de la cita
+          const citaDiv = document.createElement('div');
+          citaDiv.className = 'cita';
+          citaDiv.innerHTML = `
               <h2>Nombre: ${nombre}</h2>
-
               <p>Fecha y Hora: ${fechaHora}</p>
-
           `;
+          citasDiv.appendChild(citaDiv);
 
-      citasDiv.appendChild(citaDiv);
+          // Limpia el formulario
+          formulario.reset();
 
-      formulario.reset();
-
-    } else {
-
-      alert('Por favor, complete todos los campos.');
-
-    }
-
+          
+          setTimeout(() => {
+              citasDiv.removeChild(citaDiv);
+          }, duracionCita);
+      } else {
+          alert('Por favor, complete todos los campos.');
+      }
   });
 
+  function cambiarUsuario(nuevoUsuario) {
+      nombreInput.value = nuevoUsuario.nombre;
+      fechaHoraInput.value = nuevoUsuario.fechaHora;
+  }
+
+  setTimeout(() => {
+      const nuevoUsuario = {
+          nombre: '',
+          fechaHora: '2023-10-05T14:00'
+      };
+      cambiarUsuario(nuevoUsuario);
+  }, 5000);
 });
