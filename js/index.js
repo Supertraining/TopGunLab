ffff12982463296c3b49aee7d8323dfb4a110bc3
MatíasJMaranga <marangamatias@gmail.com>
2023-10-05 11:59:47 -0300

    functionEraseCardsDeleted

--- js/index.js
+++ js/index.js
@@ -3,18 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const formulario = document.getElementById('formulario');
   const nombreInput = document.getElementById('nombre');
   const fechaHoraInput = document.getElementById('fechaHora');
-  const duracionCita = 5000;
 
   formulario.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    // Obtiene datos del formulario
     const nombre = nombreInput.value;
     const fechaHora = fechaHoraInput.value;
 
-    // Verificación de ingreso de datos
     if (nombre && fechaHora) {
-      // Creación de la cita
+
       const citaDiv = document.createElement('div');
       citaDiv.className = 'cita';
       citaDiv.innerHTML = `
@@ -23,28 +20,11 @@ document.addEventListener('DOMContentLoaded', () => {
           `;
       citasDiv.appendChild(citaDiv);
 
-      // Limpia el formulario
       formulario.reset();
 
-
-      setTimeout(() => {
-        citasDiv.removeChild(citaDiv);
-      }, duracionCita);
     } else {
       alert('Por favor, complete todos los campos.');
     }
   });
 
-  function cambiarUsuario(nuevoUsuario) {
-    nombreInput.value = nuevoUsuario.nombre;
-    fechaHoraInput.value = nuevoUsuario.fechaHora;
-  }
-
-  setTimeout(() => {
-    const nuevoUsuario = {
-      nombre: 'Nuevo Usuario',
-      fechaHora: '2023-10-05T14:00'
-    };
-    cambiarUsuario(nuevoUsuario);
-  }, 5000);
 });
